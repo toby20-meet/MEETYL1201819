1c647c5244ebde5b55becfac42c2dcc731e760a0
toby20-meet <toby20@meet.mit.edu>
2019-01-15 20:01:16 +0200

    lablab

--- ball.py
+++ ball.py
@@ -7,8 +7,9 @@ from turtle import Turtle
 class Ball(Turtle):
 	def __init__(self,x,y,dx,dy,r,color):
 		Turtle.__init__(self)
-		self.setposition(x,y)
 		self.shape("circle")
+		self.pu()
+		self.setposition(x,y)
 		#self.x = x
 		#self.y = y
 		self.dx = dx
@@ -18,21 +19,21 @@ class Ball(Turtle):
 		self.shapesize(self.r/10)
 		self.color(color)
 	def move(self,screen_width,screen_height):
-		current_x = self.x
-		new_x = current_x + dx
-		current_y = self.y
-		new_y = current_y + dy
+		current_x = self.xcor()
+		new_x = current_x + self.dx
+		current_y = self.ycor()
+		new_y = current_y + self.dy
 		right_side_ball = new_x + self.r
 		left_side_ball = new_x - self.r
 		up_side_ball = new_y + self.r
 		down_side_ball = new_y - self.r
 		self.goto(new_x,new_y)
 		if right_side_ball >= screen_width:
-			dx = -dx
+			self.dx = -self.dx
 		if up_side_ball >= screen_height:
-			dy = -dy
+			self.dy = -self.dy
 		if left_side_ball <= -screen_width:
-			dx = -dx
+			self.dx = -self.dx
 		if down_side_ball <= -screen_height:
-			dy = -dy 
+			self.dy = -self.dy 
 # turtle.mainloop()
--- project_A.py
+++ project_A.py
@@ -1,8 +1,9 @@
 import turtle
 import time
 import random
+import math
 from ball import Ball
-turtle.tracer(0)
+turtle.tracer(0,0)
 turtle.hideturtle()
 RUNNING = True 
 SLEEP = 0.0077
@@ -10,23 +11,37 @@ SLEEP = 0.0077
 SCREEN_WIDTH = turtle.getcanvas().winfo_width()/2
 SCREEN_HEIGHT = turtle.getcanvas().winfo_height()/2
 
-NUMBER_OF_BALLS = 5
+NUMBER_OF_BALLS = 10
 MINIMUM_BALL_RADIUS = 10
-MAXIMUM_BALL_RADIUS = 100
-MINIMUM_BALL_DX = -5
-MAXIMUM_BALL_DX = 5
-MINIMUM_BALL_DY = -5
-MAXIMUM_BALL_DY = 5
+MAXIMUM_BALL_RADIUS = 50
+MINIMUM_BALL_DX = -1
+MAXIMUM_BALL_DX = 1
+MINIMUM_BALL_DY = -1
+MAXIMUM_BALL_DY = 1
 
+NUMBER_OF_FOODS = 20
+FOOD_RADIUS = 5
+
+FOODS = []
 BALLS = []
 
-MY_BALL = Ball(0,0,5,5,7,"pink")
+MY_BALL = Ball(0,0,5,0,20,"pink")
 
+for i in range(NUMBER_OF_FOODS):
+	x = random.randint(-SCREEN_WIDTH + FOOD_RADIUS, SCREEN_WIDTH - FOOD_RADIUS)
+	y = random.randint(-SCREEN_HEIGHT + FOOD_RADIUS,SCREEN_HEIGHT - FOOD_RADIUS)
+	color = (random.random(),random.random(),random.random())
+	new_food = Ball(x,y,0,0,FOOD_RADIUS,color)
+	FOODS.append(new_food)
 for i in range(NUMBER_OF_BALLS):
 	x = random.randint(-SCREEN_WIDTH + MAXIMUM_BALL_RADIUS,SCREEN_WIDTH - MAXIMUM_BALL_RADIUS)
 	y = random.randint(-SCREEN_HEIGHT + MAXIMUM_BALL_RADIUS,SCREEN_HEIGHT - MAXIMUM_BALL_RADIUS)
-	dx = random.randint(MINIMUM_BALL_DX,MAXIMUM_BALL_DX)
-	dy = random.randint(MINIMUM_BALL_DY,MAXIMUM_BALL_DY)
+	dx = 0
+	while dx == 0:
+		dx = random.uniform(MINIMUM_BALL_DX,MAXIMUM_BALL_DX)
+	dy = 0
+	while dy == 0:
+		dy = random.uniform(MINIMUM_BALL_DY,MAXIMUM_BALL_DY)
 	radius = random.randint(MINIMUM_BALL_RADIUS,MAXIMUM_BALL_RADIUS)
 	color = (random.random(),random.random(),random.random())
 	new_ball = Ball(x,y,dx,dy,radius,color)
@@ -34,13 +49,13 @@ for i in range(NUMBER_OF_BALLS):
 
 def move_all_balls():
 	for i in BALLS:
-		move(SCREEN_WIDTH,SCREEN_HEIGHT)
+		i.move(SCREEN_WIDTH,SCREEN_HEIGHT)
 
 def collide(ball_a,ball_b):
 	if ball_a == ball_b:
 		return False
-	Distance = math.sqrt(math.pow((ball_a.xcor-ball_b.xcor),2)+math.pow(ball_a.ycor-ball_b.ycor))
-	if Distance + 10 <= ball_a.r+ball_b.r:
+	Distance = math.sqrt(math.pow((ball_a.xcor()-ball_b.xcor()),2)+math.pow(ball_a.ycor()-ball_b.ycor(),2))
+	if Distance+10 <= ball_a.r+ball_b.r:
 		return True
 	else:
 		return False
@@ -48,5 +63,78 @@ def collide(ball_a,ball_b):
 def check_all_balls_collision():
 	for ball_a in BALLS:
 		for ball_b in BALLS:
-			
-
+			if collide(ball_a,ball_b):
+				radius_a = ball_a.r
+				radius_b = ball_b.r
+				x = random.randint(-SCREEN_WIDTH + MAXIMUM_BALL_RADIUS,SCREEN_WIDTH - MAXIMUM_BALL_RADIUS)
+				y = random.randint(-SCREEN_HEIGHT + MAXIMUM_BALL_RADIUS,SCREEN_HEIGHT - MAXIMUM_BALL_RADIUS)
+				dx = 0
+				while dx == 0:
+					dx = random.uniform(MINIMUM_BALL_DX,MAXIMUM_BALL_DX)
+				dy = 0
+				while dy == 0:
+					dy = random.uniform(MINIMUM_BALL_DY,MAXIMUM_BALL_DY)
+				radius = random.randint(MINIMUM_BALL_RADIUS,MAXIMUM_BALL_RADIUS)
+				color = (random.random(),random.random(),random.random())
+				if radius_a <= radius_b:
+					ball_a.setposition(x,y)
+					ball_a.dx = dx
+					ball_a.dy = dy
+					ball_a.r = radius
+					ball_a.color(color)
+					ball_b.r += 1
+					ball_a.shapesize(ball_a.r/10)
+					ball_b.shapesize(ball_b.r/10)
+				else:
+					ball_b.setposition(x,y)
+					ball_b.dx = dx
+					ball_b.dy = dy
+					ball_b.r = radius
+					ball_b.color(color)
+					ball_a.r += 1
+					ball_a.shapesize(ball_a.r/10)
+					ball_b.shapesize(ball_b.r/10)
+def check_myball_collision():
+	for ball in BALLS:
+		if collide(ball,MY_BALL):
+			radius_my = MY_BALL.r
+			radius_alien = ball.r
+			if radius_my < radius_alien:
+				print(radius_my, radius_alien)
+				print(MY_BALL.pos(), ball.pos())
+				return False
+			else:
+				x = random.randint(-SCREEN_WIDTH + MAXIMUM_BALL_RADIUS,SCREEN_WIDTH - MAXIMUM_BALL_RADIUS)
+				y = random.randint(-SCREEN_HEIGHT + MAXIMUM_BALL_RADIUS,SCREEN_HEIGHT - MAXIMUM_BALL_RADIUS)
+				dx = 0
+				while dx == 0:
+					dx = random.uniform(MINIMUM_BALL_DX,MAXIMUM_BALL_DX)
+				dy = 0
+				while dy == 0:
+					dy = random.uniform(MINIMUM_BALL_DY,MAXIMUM_BALL_DY)
+				radius = random.randint(MINIMUM_BALL_RADIUS,MAXIMUM_BALL_RADIUS)
+				color = (random.random(),random.random(),random.random())
+				ball.setposition(x,y)
+				ball.dx = dx
+				ball.dy = dy
+				ball.r = radius
+				ball.color(color)
+				MY_BALL.r += 1
+				MY_BALL.shapesize(MY_BALL.r/10)
+				ball.shapesize(ball.r/10)
+	return True
+def move_around(event):
+	MY_BALL.goto(event.x - SCREEN_WIDTH, SCREEN_HEIGHT - event.y)
+turtle.getcanvas().bind("<Motion>", move_around)
+turtle.listen()
+while RUNNING:
+	RUNNING = check_myball_collision()
+	if SCREEN_WIDTH != turtle.getcanvas().winfo_width()/2 or SCREEN_HEIGHT != turtle.getcanvas().winfo_height()/2:
+		SCREEN_WIDTH = int(turtle.getcanvas().winfo_width()/2)
+		SCREEN_HEIGHT = int(turtle.getcanvas().winfo_height()/2)
+	move_all_balls()
+	check_all_balls_collision()
+	check_myball_collision()
+	turtle.update()
+	time.sleep(SLEEP)
+#add food and viruses
